fe9c64e9a3ec3accb71da2902212adad668a4ef6
dvJoshr <joseph.64@outlook.com>
2023-10-25 20:20:10 -0500

    Cambio de variables

--- dist/app.js
+++ dist/app.js
@@ -18,6 +18,19 @@ const allowedOrigins = [
 const options = {
     origin: allowedOrigins,
 };
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", 1);
+    // Pass to next layer of middleware
+    next();
+});
 app.use((0, cors_1.default)(options));
 app.use(express_1.default.json());
 // app.use((req, res, next) => {
